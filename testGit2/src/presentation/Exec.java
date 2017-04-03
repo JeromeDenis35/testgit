79ccc2b19b0853778ea00f3dde4db454f0211723
Melina Lamy <lamymel4@yahoo.fr>
2017-04-03 15:05:39 +0200

    deuxième mise à jour avec un nouveau package

---
+++ testGit2/src/presentation/Exec.java
@@ -0,0 +1,13 @@
+package presentation;
+
+import testGit2.Jerome;
+
+public class Exec {
+
+	public static void main(String[] args) {
+		Jerome j=new Jerome("Denis", "Jerome");
+		j.coucou();
+
+	}
+
+}
