938c6661d73e5aefe4438f0b4f6ff0ff8962e9ee
Melina Lamy <lamymel4@yahoo.fr>
2017-04-03 14:46:51 +0200

    Mis à jour du 03/04/17 J'ai rempli la classe

--- testGit2/src/testGit2/Jerome.java
+++ testGit2/src/testGit2/Jerome.java
@@ -1,7 +1,44 @@
 package testGit2;
 
 public class Jerome {
+	
+	//petite modification dans mon code !!!
+	protected String nom;
+	protected String prenom;
+	
+	public Jerome() {
+	}
 
+	public Jerome(String nom, String prenom) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+	
+	public void coucou(){
+		System.out.println("hello tout le monde ! ");
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	
+	
+	
+	
 	
 	
 	
